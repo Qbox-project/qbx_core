a97836560f9658b6351f3907eb57558fbd9030f7
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-10-26 15:09:49 +0200

    feat(server/main): esx export compat

--- server/main.lua
+++ server/main.lua
@@ -11,3 +11,9 @@ end)
 -- To use this export in a script instead of manifest method
 -- Just put this line of code below at the very top of the script
 -- local QBCore = exports['qb-core']:GetCoreObject()
+
+AddEventHandler('__cfx_export_es_extended_getSharedObject', function(setCB)
+    setCB(function()
+        return QBCore
+    end)
+end)
