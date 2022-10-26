9c8707d0705cfa0f7dd32bd338132080ff908348
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-10-26 15:23:36 +0200

    tweak(client/functions): keep name as it is

--- client/functions.lua
+++ client/functions.lua
@@ -135,7 +135,7 @@ end
 function QBCore.Functions.Progressbar(name, label, duration, useWhileDead, canCancel, disableControls, animation, prop, propTwo, onFinish, onCancel)
     if GetResourceState('progressbar') ~= 'started' then error('progressbar needs to be started in order for QBCore.Functions.Progressbar to work') end
     exports['progressbar']:Progress({
-        name = name:lower(),
+        name = name,
         duration = duration,
         label = label,
         useWhileDead = useWhileDead,
