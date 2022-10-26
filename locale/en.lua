66ae5c6cbc153a7e9d7a1a979d4acf3d45ca397c
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-10-26 13:14:27 +0200

    tweak(locale/en): take convar into account

--- locale/en.lua
+++ locale/en.lua
@@ -120,7 +120,10 @@ local Translations = {
     },
 }
 
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'en' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
