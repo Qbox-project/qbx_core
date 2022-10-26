896bf1c425906beed8245a78c0e982a5c8efc0d3
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-10-26 17:25:56 +0200

    chore(client/loops): formatting

--- client/loops.lua
+++ client/loops.lua
@@ -4,7 +4,7 @@ CreateThread(function()
         Wait(timeout)
 
         if IsLoggedIn then
-            if (QBCore.PlayerData.metadata['hunger'] <= 0 or QBCore.PlayerData.metadata['thirst'] <= 0) and not QBCore.PlayerData.metadata['isdead'] then
+            if (QBCore.PlayerData.metadata.hunger <= 0 or QBCore.PlayerData.metadata.thirst <= 0) and not QBCore.PlayerData.metadata.isdead then
                 local ped = PlayerPedId()
                 local currentHealth = GetEntityHealth(ped)
                 local decreaseThreshold = math.random(5, 10)
