61b9206442d7fa7d4232ee6f8cc46d573e84b618
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-10-28 05:55:07 +0200

    Update events.lua

--- client/events.lua
+++ client/events.lua
@@ -168,8 +168,8 @@ RegisterNetEvent('QBCore:Player:UpdatePlayerData', function()
     TriggerServerEvent('QBCore:UpdatePlayer')
 end)
 
-RegisterNetEvent('QBCore:Notify', function(text, type, length)
-    QBCore.Functions.Notify(text, type, length)
+RegisterNetEvent('QBCore:Notify', function(text, notifytype, length)
+    QBCore.Functions.Notify(text, notifytype, length)
 end)
 
 -- This event is exploitable and should not be used. It has been deprecated, and will be removed soon.
