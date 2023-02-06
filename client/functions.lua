802aae4f2fdfa737640fd7ec24a4716858ec6fb3
Thijmen <53520119+legende11@users.noreply.github.com>
2023-02-06 12:48:47 +0100

    refractor(client) closest vehicle to ox

--- client/functions.lua
+++ client/functions.lua
@@ -304,24 +304,14 @@ end
 
 function QBCore.Functions.GetClosestVehicle(coords)
     local ped = PlayerPedId()
-    local vehicles = GetGamePool('CVehicle')
-    local closestDistance = -1
-    local closestVehicle = -1
     if coords then
         coords = type(coords) == 'table' and vec3(coords.x, coords.y, coords.z) or coords
     else
         coords = GetEntityCoords(ped)
     end
-    for i = 1, #vehicles, 1 do
-        local vehicleCoords = GetEntityCoords(vehicles[i])
-        local distance = #(vehicleCoords - coords)
-
-        if closestDistance == -1 or closestDistance > distance then
-            closestVehicle = vehicles[i]
-            closestDistance = distance
-        end
-    end
-    return closestVehicle, closestDistance
+    local Vehicle, VehicleCoords = lib.getClosestVehicle(coords, 50, true)
+    local closestDistance = #(VehicleCoords - coords)
+    return Vehicle, closestDistance
 end
 
 function QBCore.Functions.GetClosestObject(coords)
