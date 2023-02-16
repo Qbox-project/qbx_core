eb0e2f1ec159780ab405124cf611af86ab4bdb2e
Wisfakim <marceau.rvr@gmail.com>
2023-02-16 04:14:56 +0100

    Add CreateVehicleWithType Revert CreateVehicle

--- server/functions.lua
+++ server/functions.lua
@@ -184,6 +184,25 @@ function QBCore.Functions.SpawnVehicle(source, model, coords, warp)
     while NetworkGetEntityOwner(veh) ~= source do Wait(0) end
     return veh
 end
+
+-- Server side vehicle creation with optional callback
+-- The CreateVehicleServerSetter native uses only the server to create a vehicle instead of using the client as well
+function QBCore.Functions.CreateVehicle(source, model, coords, warp)
+    model = type(model) == 'string' and joaat(model) or model
+    if not coords then coords = GetEntityCoords(GetPlayerPed(source)) end
+    if not CreateVehicleServerSetter then
+        error('^1CreateVehicleServerSetter is not available on your artifact, please use artifact 5904 or above to be able to use this^0')
+        return
+    end
+    local tempVehicle = CreateVehicle(model, 0, 0, 0, 0, true, true)
+    while not DoesEntityExist(tempVehicle) do Wait(0) end
+    local vehicleType = GetVehicleType(tempVehicle)
+    DeleteEntity(tempVehicle)
+    local veh = CreateVehicleServerSetter(model, vehicleType, coords.x, coords.y, coords.z, coords.w)
+    while not DoesEntityExist(veh) do Wait(0) end
+    if warp then TaskWarpPedIntoVehicle(GetPlayerPed(source), veh, -1) end
+    return veh
+end
 -- Vehicle types
 -- automobile
 -- bike
@@ -193,19 +212,13 @@ end
 -- submarine
 -- trailer
 -- train
--- Server side vehicle creation with optional callback
--- The CreateVehicleServerSetter native uses only the server to create a vehicle instead of using the client as well
-function QBCore.Functions.CreateVehicle(source, model, coords, warp, vehicleType)
+function QBCore.Functions.CreateVehicleWithType(source, model, coords, warp, vehicleType)
     model = type(model) == 'string' and joaat(model) or model
     if not coords then coords = GetEntityCoords(GetPlayerPed(source)) end
     if not CreateVehicleServerSetter then
         error('^1CreateVehicleServerSetter is not available on your artifact, please use artifact 5904 or above to be able to use this^0')
         return
     end
-    --local tempVehicle = CreateVehicle(model, 0, 0, 0, 0, true, true)
-    --while not DoesEntityExist(tempVehicle) do Wait(0) end
-    --local vehicleType = GetVehicleType(tempVehicle)
-    --DeleteEntity(tempVehicle)
     local veh = CreateVehicleServerSetter(model, vehicleType, coords.x, coords.y, coords.z, coords.w)
     while not DoesEntityExist(veh) do Wait(0) end
     if warp then TaskWarpPedIntoVehicle(GetPlayerPed(source), veh, -1) end
