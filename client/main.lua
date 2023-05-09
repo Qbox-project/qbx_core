c35b528969f971543fc869f6469437ec0a0ac0f0
manason <clanerp@gmail.com>
2023-05-09 08:18:57 -0700

    chore: deprecating qb callback handling in favor of ox_lib callback functions

--- client/main.lua
+++ client/main.lua
@@ -2,8 +2,13 @@ QBCore = {}
 QBCore.PlayerData = {}
 QBCore.Config = QBConfig
 QBCore.Shared = QBShared
+
+---@deprecated use https://overextended.github.io/docs/ox_lib/Callback/Lua/Client/ instead
 QBCore.ClientCallbacks = {}
+
+---@deprecated use https://overextended.github.io/docs/ox_lib/Callback/Lua/Client/ instead
 QBCore.ServerCallbacks = {}
+
 IsLoggedIn = false
 
 exports('GetCoreObject', function()
