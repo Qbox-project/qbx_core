b4ab09791b38d964489e9e9c1c1a281d31162522
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-06-14 11:15:53 +0200

    tweak(shared/main): change to new util functions

--- shared/main.lua
+++ shared/main.lua
@@ -1,200 +1,40 @@
 QBShared = QBShared or {}
 
-local StringCharset = {}
-local NumberCharset = {}
-
-for i = 48, 57 do NumberCharset[#NumberCharset + 1] = string.char(i) end
-for i = 65, 90 do StringCharset[#StringCharset + 1] = string.char(i) end
-for i = 97, 122 do StringCharset[#StringCharset + 1] = string.char(i) end
+QBShared.StarterItems = {
+    phone = { amount = 1, item = 'phone' },
+    id_card = { amount = 1, item = 'id_card' },
+    driver_license = { amount = 1, item = 'driver_license' },
+}
 
 ---@deprecated use CommaValue from imports/utils.lua
----converts a number to a string version with commas
----@param num number
----@return string
-function QBShared.CommaValue(num)
-    local formatted = tostring(num)
-    local numChanged
-    repeat
-        formatted, numChanged = string.gsub(formatted, '^(-?%d+)(%d%d%d)', '%1,%2')
-    until numChanged == 0
-    return formatted
-end
+QBShared.CommaValue = CommaValue
 
 ---@deprecated use RandomLetter from imports/utils.lua
----@param length integer
----@return string
-function QBShared.RandomStr(length)
-    if length <= 0 then return '' end
-    return QBShared.RandomStr(length - 1) .. StringCharset[math.random(1, #StringCharset)]
-end
+QBShared.RandomStr = RandomLetter
 
 ---@deprecated use RandomNumber from imports/utils.lua
----@param length integer
----@return string
-function QBShared.RandomInt(length)
-    if length <= 0 then return '' end
-    return QBShared.RandomInt(length - 1) .. NumberCharset[math.random(1, #NumberCharset)]
-end
+QBShared.RandomInt = RandomNumber
 
 ---@deprecated use string.split from imports/utils.lua
----@param str string
----@param delimiter string character
----@return string[]
-function QBShared.SplitStr(str, delimiter)
-    local result = {}
-    local from = 1
-    local delim_from, delim_to = string.find(str, delimiter, from)
-    while delim_from do
-        result[#result + 1] = string.sub(str, from, delim_from - 1)
-        from = delim_to + 1
-        delim_from, delim_to = string.find(str, delimiter, from)
-    end
-    result[#result + 1] = string.sub(str, from)
-    return result
-end
+QBShared.SplitStr = string.split
 
 ---@deprecated use string.trim from imports/utils.lua
----@param value string
----@return string?
----@return number? count
-function QBShared.Trim(value)
-    if not value then return end
-    return string.gsub(value, '^%s*(.-)%s*$', '%1')
-end
+QBShared.Trim = string.trim
 
 ---@deprecated use string.firstToUpper from imports/utils.lua
----@param value string
----@return string?
-function QBShared.FirstToUpper(value)
-    if not value then return end
-    return value:gsub("^%l", string.upper)
-end
+QBShared.FirstToUpper = string.firstToUpper
 
 ---@deprecated use math.round from imports/utils.lua
----@param value number
----@param numDecimalPlaces integer
----@return integer
-function QBShared.Round(value, numDecimalPlaces)
-    if not numDecimalPlaces then return math.floor(value + 0.5) end
-    local power = 10 ^ numDecimalPlaces
-    return math.floor((value * power) + 0.5) / (power)
-end
+QBShared.Round = math.round
 
 ---@deprecated use ChangeVehicleExtra from imports/utils.lua
----@param vehicle number
----@param extra number
----@param enable boolean
-function QBShared.ChangeVehicleExtra(vehicle, extra, enable)
-    if not DoesExtraExist(vehicle, extra) then return end
-
-    SetVehicleExtra(vehicle, extra, not enable)
-    local isExtraOn = IsVehicleExtraTurnedOn(vehicle, extra)
-    if enable ~= isExtraOn then
-        QBShared.ChangeVehicleExtra(vehicle, extra, enable)
-    end
-end
+QBShared.ChangeVehicleExtra = ChangeVehicleExtra
 
 ---@deprecated use SetVehicleExtras from imports/utils.lua
----@param vehicle number
----@param config table
-function QBShared.SetDefaultVehicleExtras(vehicle, config)
-    -- Clear Extras
-    for i = 1, 20 do
-        if DoesExtraExist(vehicle, i) then
-            SetVehicleExtra(vehicle, i, true)
-        end
-    end
-
-    for id, enabled in pairs(config) do
-        QBShared.ChangeVehicleExtra(vehicle, tonumber(id), type(enabled) == 'boolean' and enabled or true)
-    end
-end
-
-QBShared.StarterItems = {
-    ['phone'] = { amount = 1, item = 'phone' },
-    ['id_card'] = { amount = 1, item = 'id_card' },
-    ['driver_license'] = { amount = 1, item = 'driver_license' },
-}
+QBShared.SetDefaultVehicleExtras = SetVehicleExtras
 
 ---@deprecated use MaleNoGloves from imports/utils.lua
-QBShared.MaleNoGloves = {
-    [0] = true,
-    [1] = true,
-    [2] = true,
-    [3] = true,
-    [4] = true,
-    [5] = true,
-    [6] = true,
-    [7] = true,
-    [8] = true,
-    [9] = true,
-    [10] = true,
-    [11] = true,
-    [12] = true,
-    [13] = true,
-    [14] = true,
-    [15] = true,
-    [18] = true,
-    [26] = true,
-    [52] = true,
-    [53] = true,
-    [54] = true,
-    [55] = true,
-    [56] = true,
-    [57] = true,
-    [58] = true,
-    [59] = true,
-    [60] = true,
-    [61] = true,
-    [62] = true,
-    [112] = true,
-    [113] = true,
-    [114] = true,
-    [118] = true,
-    [125] = true,
-    [132] = true
-}
+QBShared.MaleNoGloves = MaleNoGloves
 
 ---@deprecated use FemaleNoGloves from imports/utils.lua
-QBShared.FemaleNoGloves = {
-    [0] = true,
-    [1] = true,
-    [2] = true,
-    [3] = true,
-    [4] = true,
-    [5] = true,
-    [6] = true,
-    [7] = true,
-    [8] = true,
-    [9] = true,
-    [10] = true,
-    [11] = true,
-    [12] = true,
-    [13] = true,
-    [14] = true,
-    [15] = true,
-    [19] = true,
-    [59] = true,
-    [60] = true,
-    [61] = true,
-    [62] = true,
-    [63] = true,
-    [64] = true,
-    [65] = true,
-    [66] = true,
-    [67] = true,
-    [68] = true,
-    [69] = true,
-    [70] = true,
-    [71] = true,
-    [129] = true,
-    [130] = true,
-    [131] = true,
-    [135] = true,
-    [142] = true,
-    [149] = true,
-    [153] = true,
-    [157] = true,
-    [161] = true,
-    [165] = true
-}
+QBShared.FemaleNoGloves = FemaleNoGloves
