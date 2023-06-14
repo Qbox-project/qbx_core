517e19c5009dadb7ba4a8dc71611e5b0f9d0d369
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-06-14 11:14:11 +0200

    tweak(server/debug): change to new util functions

--- server/debug.lua
+++ server/debug.lua
@@ -1,33 +1,5 @@
----prints tbl
 ---@deprecated
----@param tbl any
----@param indent integer
-local function tPrint(tbl, indent)
-    indent = indent or 0
-    if type(tbl) == 'table' then
-        for k, v in pairs(tbl) do
-            local tblType = type(v)
-            local formatting = ("%s ^3%s:^0"):format(string.rep("  ", indent), k)
-
-            if tblType == "table" then
-                print(formatting)
-                tPrint(v, indent + 1)
-            elseif tblType == 'boolean' then
-                print(("%s^1 %s ^0"):format(formatting, v))
-            elseif tblType == "function" then
-                print(("%s^9 %s ^0"):format(formatting, v))
-            elseif tblType == 'number' then
-                print(("%s^5 %s ^0"):format(formatting, v))
-            elseif tblType == 'string' then
-                print(("%s ^2'%s' ^0"):format(formatting, v))
-            else
-                print(("%s^2 %s ^0"):format(formatting, v))
-            end
-        end
-    else
-        print(("%s ^0%s"):format(string.rep("  ", indent), tbl))
-    end
-end
+local tPrint = DebugPrint
 
 ---prints invoking resource, and tbl.
 ---@deprecated
@@ -40,24 +12,19 @@ RegisterServerEvent('QBCore:DebugSomething', function(tbl, indent)
     print('\x1b[4m\x1b[36m[ END DEBUG ]\x1b[0m')
 end)
 
----prints invoking resource, and tbl
 ---@deprecated
----@param tbl any
----@param indent integer
-function QBCore.Debug(tbl, indent)
-    TriggerEvent('QBCore:DebugSomething', tbl, indent)
-end
+QBCore.Debug = DebugPrint
 
 ---@deprecated
 ---@param resource string
 ---@param msg string
 function QBCore.ShowError(resource, msg)
-    print(('\x1b[31m[%s:ERROR]\x1b[0m %s'):format(resource, msg))
+    DebugPrint(('\x1b[31m[%s:ERROR]\x1b[0m %s'):format(resource, msg))
 end
 
 ---@deprecated
 ---@param resource string
 ---@param msg string
 function QBCore.ShowSuccess(resource, msg)
-    print(('\x1b[32m[%s:LOG]\x1b[0m %s'):format(resource, msg))
+    DebugPrint(('\x1b[32m[%s:LOG]\x1b[0m %s'):format(resource, msg))
 end
