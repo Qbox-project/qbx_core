db36b707da19adbb8949115dc2775a9a60ed218e
Snipe <10562121+pushkart2@users.noreply.github.com>
2023-07-06 08:24:41 -0500

    Add Toggle Duty events when duty is changed.
    - A lot of other scripts listen to the toggle duty events instead of getting the whole playerdata object to get the latest duty status.

--- server/player.lua
+++ server/player.lua
@@ -296,6 +296,8 @@ function QBCore.Player.CreatePlayer(PlayerData, Offline)
     ---@param onDuty boolean
     function self.Functions.SetJobDuty(onDuty)
         self.PlayerData.job.onduty = not not onDuty -- Make sure the value is a boolean if nil is sent
+        TriggerEvent('QBCore:Server:SetDuty', self.PlayerData.source, self.PlayerData.job.onduty)
+        TriggerClientEvent('QBCore:Client:SetDuty', self.PlayerData.source, self.PlayerData.job.onduty)
         self.Functions.UpdatePlayerData()
     end
 
