7b5222beb69ca9ee3681f0a8703b1ca01ebbf1ea
unknown <clanerp@gmail.com>
2023-09-12 22:20:39 -0700

    chore: deprecate core exports in favor of module import

--- server/main.lua
+++ server/main.lua
@@ -8,6 +8,7 @@ QBCore.ClientCallbacks = {}
 ---@deprecated use https://overextended.github.io/docs/ox_lib/Callback/Lua/Server instead
 QBCore.ServerCallbacks = {}
 
+---@deprecated import QBCore using module 'qbx-core:core' https://qbox-docs.vercel.app/resources/core/import
 exports('GetCoreObject', function()
     return QBCore
 end)
@@ -16,6 +17,7 @@ end)
 -- Just put this line of code below at the very top of the script
 -- local QBCore = exports['qbx-core']:GetCoreObject()
 
+---@deprecated import QBCore using module 'qbx-core:core' https://qbox-docs.vercel.app/resources/core/import
 AddEventHandler('__cfx_export_qb-core_GetCoreObject', function(setCB)
     setCB(function()
         return QBCore
