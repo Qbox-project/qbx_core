9e90b5e753537b87daf84738ca38ea634ddab4f6
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-09-25 22:57:12 +0200

    tweak(client/main): add back export

--- client/main.lua
+++ client/main.lua
@@ -5,3 +5,6 @@ QBCore.Shared = require 'shared.main'
 QBCore.Functions = require 'client.functions'
 
 IsLoggedIn = false
+
+---import QBX using module 'qbx-core:core' https://qbox-project.github.io/resources/core/import
+exports('GetCoreObject', function() return QBCore end)
