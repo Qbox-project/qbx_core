33f02ac1dd77f4eea8971b9a37e83423041d3796
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-09-25 23:36:20 +0200

    feat(modules/core): isLoggedIn

--- modules/core.lua
+++ modules/core.lua
@@ -1,1 +1,6 @@
 QBX = exports.qbx_core:GetCoreObject()
+QBX.IsLoggedIn = LocalPlayer.state.isLoggedIn
+
+AddStateBagChangeHandler('isLoggedIn', ('player:%s'):format(cache.serverId), function(_, _, value)
+    QBX.IsLoggedIn = value
+end)
