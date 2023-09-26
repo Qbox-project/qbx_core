c74d7f3872f6b937746860fd4fdba33331dadc91
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-09-26 22:56:07 +0200

    fix(actions): bump version

--- .github/actions/bump-manifest-version.js
+++ .github/actions/bump-manifest-version.js
@@ -1,11 +1,11 @@
 // Based off of https://github.com/overextended/ox_lib/blob/master/.github/actions/bump-manifest-version.js
-import { readFileSync, writeFileSync } from 'fs'
+const fs = require('fs')
 
 const version = process.env.TGT_RELEASE_VERSION
 const newVersion = version.replace('v', '')
 
-const manifestFile = readFileSync('fxmanifest.lua', {encoding: 'utf8'})
+const manifestFile = fs.readFileSync('fxmanifest.lua', {encoding: 'utf8'})
 
 const newFileContent = manifestFile.replace(/\bversion\s+(.*)$/gm, `version '${newVersion}'`)
 
-writeFileSync('fxmanifest.lua', newFileContent)
+fs.writeFileSync('fxmanifest.lua', newFileContent)
