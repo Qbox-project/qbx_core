2273466fb49939b9a5338b9105fba39f063f14fe
Manason <clanerp@gmail.com>
2023-09-26 07:48:19 -0700

    init isLoggedIn to false

--- modules/core.lua
+++ modules/core.lua
@@ -1,7 +1,6 @@
 QBX = exports.qbx_core:GetCoreObject()
 
 if not IsDuplicityVersion() then
-    QBX.IsLoggedIn = LocalPlayer.state.isLoggedIn
     AddStateBagChangeHandler('isLoggedIn', ('player:%s'):format(cache.serverId), function(_, _, value)
         QBX.IsLoggedIn = value
     end)
