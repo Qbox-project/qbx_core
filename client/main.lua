53a37df0e273ce37887d3caf4fcb6e7289667465
unknown <clanerp@gmail.com>
2023-09-27 02:07:40 -0700

    refactor: accessor exports for shared data tables

--- client/main.lua
+++ client/main.lua
@@ -3,5 +3,29 @@ QBX.PlayerData = {}
 QBX.Shared = require 'shared.main'
 QBX.IsLoggedIn = false
 
+function GetJobs()
+    return QBX.Shared.Jobs
+end
+
+exports('GetJobs', GetJobs)
+
+function GetGangs()
+    return QBX.Shared.Gangs
+end
+
+exports('GetGangs', GetGangs)
+
+function GetVehiclesByName()
+    return QBX.Shared.Vehicles
+end
+
+exports('GetVehiclesByName', GetVehiclesByName)
+
+function GetVehiclesByHash()
+    return QBX.Shared.VehicleHashes
+end
+
+exports('GetVehiclesByHash', GetVehiclesByHash)
+
 ---@deprecated import QBX using module 'qbx_core:core' https://qbox-project.github.io/resources/core/import
 exports('GetCoreObject', function() return QBX end)
--- server/main.lua
+++ server/main.lua
@@ -86,5 +86,29 @@ end
 
 exports('RemoveGang', RemoveGang)
 
+function GetJobs()
+    return QBX.Shared.Jobs
+end
+
+exports('GetJobs', GetJobs)
+
+function GetGangs()
+    return QBX.Shared.Gangs
+end
+
+exports('GetGangs', GetGangs)
+
+function GetVehiclesByName()
+    return QBX.Shared.Vehicles
+end
+
+exports('GetVehiclesByName', GetVehiclesByName)
+
+function GetVehiclesByHash()
+    return QBX.Shared.VehicleHashes
+end
+
+exports('GetVehiclesByHash', GetVehiclesByHash)
+
 ---@deprecated import QBX using module 'qbx_core:core' https://qbox-project.github.io/resources/core/import
 exports('GetCoreObject', function() return QBX end)
