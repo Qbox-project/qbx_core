667296ddaa31a307f352f177acab6d9e470577a5
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2023-10-10 16:31:00 +0200

    add Progressbar
    I know that you can get the progress bar from ox_lib, but this is just better, not everyone looks at the ox docs and a core does need a progress bar ps not hate me xd

--- client/functions.lua
+++ client/functions.lua
@@ -35,6 +35,42 @@ end
 
 exports('Notify', Notify)
 
+
+function Progressbar(_, label, duration, useWhileDead, canCancel, disableControls, animation, prop, _, onFinish, onCancel)
+    if lib.progressBar({
+        duration = duration,
+        label = label,
+        useWhileDead = useWhileDead,
+        canCancel = canCancel,
+        disable = {
+            move = disableControls?.disableMovement,
+            car = disableControls?.disableCarMovement,
+            combat = disableControls?.disableCombat,
+            mouse = disableControls?.disableMouse,
+        },
+        anim = {
+            dict = animation?.animDict,
+            clip = animation?.anim,
+            flags = animation?.flags
+        },
+        prop = {
+            model = prop?.model,
+            pos = prop?.coords,
+            rot = prop?.rotation,
+        },
+    }) then
+        if onFinish then
+            onFinish()
+        end
+    else
+        if onCancel then
+            onCancel()
+        end
+    end
+end
+exports('Progressbar', Progressbar)
+
+
 ---@return PlayerData? playerData
 function GetPlayerData()
     return QBX.PlayerData
