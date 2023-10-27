70bce5c072b4014f0c90cf9f50039e8274b27d6e
Manason <clanerp@gmail.com>
2023-10-26 23:50:37 -0700

    refactor(server/loops): re-organized using guard clauses

--- server/loops.lua
+++ server/loops.lua
@@ -17,31 +17,35 @@ lib.cron.new(('*/%s * * * *'):format(Config.UpdateInterval), function()
     end
 end)
 
+local function sendPaycheck(player, payment)
+    player.Functions.AddMoney('bank', payment)
+    TriggerClientEvent('QBCore:Notify', player.PlayerData.source, Lang:t('info.received_paycheck', {value = payment}))
+end
+
+local function pay(player)
+    local job = player.PlayerData.job
+    local payment = QBX.Shared.Jobs[job.name].grades[job.grade.level].payment or job.payment
+    if payment <= 0 then return end
+    if not QBX.Shared.Jobs[job.name].offDutyPay and not job.onduty then return end
+    if not Config.Money.PaycheckSociety then
+        sendPaycheck(player, payment)
+        return
+    end
+    local account = exports['qbx_management']:GetAccount(job.name)
+    if not account or account == 0 then -- Checks if player is employed by a society
+        sendPaycheck(player, payment)
+        return
+    end
+    if account < payment then -- Checks if company has enough money to pay society
+        TriggerClientEvent('QBCore:Notify', player.PlayerData.source, Lang:t('error.company_too_poor'), 'error')
+        return
+    end
+    exports['qbx_management']:RemoveMoney(job.name, payment)
+    sendPaycheck(player, payment)
+end
+
 lib.cron.new(('*/%s * * * *'):format(Config.Money.PaycheckTimeout), function()
     for _, player in pairs(QBX.Players) do
-        if player then
-            local payment = QBX.Shared.Jobs[player.PlayerData.job.name].grades[player.PlayerData.job.grade.level].payment
-            if not payment then payment = player.PlayerData.job.payment end
-            if player.PlayerData.job and payment > 0 and (QBX.Shared.Jobs[player.PlayerData.job.name].offDutyPay or player.PlayerData.job.onduty) then
-                if Config.Money.PaycheckSociety then
-                    local account = exports['qbx_management']:GetAccount(player.PlayerData.job.name)
-                    if account ~= 0 then -- Checks if player is employed by a society
-                        if account < payment then -- Checks if company has enough money to pay society
-                            TriggerClientEvent('QBCore:Notify', player.PlayerData.source, Lang:t('error.company_too_poor'), 'error')
-                        else
-                            player.Functions.AddMoney('bank', payment)
-                            exports['qbx_management']:RemoveMoney(player.PlayerData.job.name, payment)
-                            TriggerClientEvent('QBCore:Notify', player.PlayerData.source, Lang:t('info.received_paycheck', {value = payment}))
-                        end
-                    else
-                        player.Functions.AddMoney('bank', payment)
-                        TriggerClientEvent('QBCore:Notify', player.PlayerData.source, Lang:t('info.received_paycheck', {value = payment}))
-                    end
-                else
-                    player.Functions.AddMoney('bank', payment)
-                    TriggerClientEvent('QBCore:Notify', player.PlayerData.source, Lang:t('info.received_paycheck', {value = payment}))
-                end
-            end
-        end
+        pay(player)
     end
 end)
