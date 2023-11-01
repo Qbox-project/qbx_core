7a28f22f9931609a6b8166f39fba04e76eab3a59
Manason <clanerp@gmail.com>
2023-10-31 21:57:10 -0700

    feat(bridge/qb): add vehicle events

--- bridge/qb/client/events.lua
+++ bridge/qb/client/events.lua
@@ -3,3 +3,24 @@
 RegisterNetEvent('QBCore:Command:CallCommand', function(command)
     ExecuteCommand(command)
 end)
+
+RegisterNetEvent('QBCore:Client:VehicleInfo', function(info)
+    local plate = GetPlate(info.vehicle)
+    local hasKeys = true
+
+    if GetResourceState('qb-vehiclekeys') == 'started' then
+        hasKeys = exports['qb-vehiclekeys']:HasKeys()
+    end
+
+    local data = {
+        vehicle = info.vehicle,
+        seat = info.seat,
+        name = info.modelName,
+        plate = plate,
+        driver = GetPedInVehicleSeat(info.vehicle, -1),
+        inseat = GetPedInVehicleSeat(info.vehicle, info.seat),
+        haskeys = hasKeys
+    }
+
+    TriggerEvent('QBCore:Client:'..info.event..'Vehicle', data)
+end)
