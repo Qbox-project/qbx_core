2f735cfcf7f10b961a0c894c04e97c49317ea83e
Manason <clanerp@gmail.com>
2023-11-01 23:13:45 -0700

    fix(bridge/qb/server/functions): renamed GetDutyCountJob to GetDutyCount
    * fix(bridge/qb/server/functions): renamed GetDutyCountJob to GetDutyCount
    
    * remove GetDutyCountType from bridge as this isn't a qbcore function

--- bridge/qb/server/functions.lua
+++ bridge/qb/server/functions.lua
@@ -487,16 +487,8 @@ end
 ---@param job string name
 ---@return integer
 ---@return Source[]
-function functions.GetDutyCountJob(job)
+function functions.GetDutyCount(job)
     return exports.qbx_core:GetDutyCountJob(job)
-end
-
----Gets a list of all on duty players of a specified job type and the number
----@param type string
----@return integer
----@return Source[]
-function functions.GetDutyCountType(type)
-    return exports.qbx_core:GetDutyCountType(type)
 end
 
 -- Returns the objects related to buckets, first returned value is the player buckets, second one is entity buckets
