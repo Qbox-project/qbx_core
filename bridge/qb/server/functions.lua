ab8e061f78853041f0068a39d11c40e47c11d1a5
Manason <clanerp@gmail.com>
2023-11-12 19:14:46 -0800

    fix: bridge SpawnVehicle uses CreateVehicle
    * fix: bridge SpawnVehicle uses CreateVehicle to avoid incompatiblities with server side vehicle spawning and qb
    
    * fix: lint

--- bridge/qb/server/functions.lua
+++ bridge/qb/server/functions.lua
@@ -48,15 +48,27 @@ end
 ---@deprecated use SpawnVehicle from imports/utils.lua
 ---@return number?
 function functions.SpawnVehicle(source, model, coords, warp)
-    local netId = SpawnVehicle(source, model, coords, warp)
-    if not netId then return end
-    local veh = NetworkGetEntityFromNetworkId(netId)
+    local ped = GetPlayerPed(source)
+    model = type(model) == 'string' and joaat(model) or model
+    if not coords then coords = GetEntityCoords(ped) end
+    local heading = coords.w and coords.w or 0.0
+    local veh = CreateVehicle(model, coords.x, coords.y, coords.z, heading, true, true)
+    while not DoesEntityExist(veh) do Wait(0) end
+    if warp then
+        while GetVehiclePedIsIn(ped) ~= veh do
+            Wait(0)
+            TaskWarpPedIntoVehicle(ped, veh, -1)
+        end
+    end
     while NetworkGetEntityOwner(veh) ~= source do Wait(0) end
     return veh
 end
 
 ---@deprecated use SpawnVehicle from imports/utils.lua
-functions.CreateVehicle = functions.SpawnVehicle
+function functions.CreateVehicle(source, model, _, coords, warp)
+    local netId = SpawnVehicle(source, model, coords, warp)
+    return NetworkGetEntityFromNetworkId(netId)
+end
 
 ---@deprecated No replacement. See https://overextended.dev/ox_inventory/Functions/Client#useitem
 ---@param source Source
