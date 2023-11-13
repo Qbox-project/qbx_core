5c2344013406362d4119c70941b4d68474feafba
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2023-11-13 02:44:54 +0100

    feat(lockdown): use inactive lockdown mode as default

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,5 @@
 ---@type 'strict'|'relaxed'|'inactive'
-local bucketLockDownMode = GetConvar('qbx:bucketlockdownmode', 'relaxed')
+local bucketLockDownMode = GetConvar('qbx:bucketlockdownmode', 'inactive')
 SetRoutingBucketEntityLockdownMode(0, bucketLockDownMode)
 
 if not lib.checkDependency('ox_lib', '3.10.0', true) then error() return end
