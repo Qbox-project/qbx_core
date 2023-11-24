b9e95eb8c66f81f849fbfa0913ccf4a9226d2d41
shanruto <dalavanja025@gmail.com>
2023-11-24 20:33:29 +0100

    Update config/client.lua
    Co-authored-by: Solareon <769465+solareon@users.noreply.github.com>

--- config/client.lua
+++ config/client.lua
@@ -68,6 +68,6 @@ return {
 
     --- Only used by QB bridge
     hasKeys = function()
-        return exports['qbx_vehiclekeys']:HasKeys()
+        return exports.qbx_vehiclekeys:HasKeys()
     end,
 }
