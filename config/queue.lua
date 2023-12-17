a7bcbc9e894fa388186c71355ddb4b395de31098
David Malchin <malchin459@gmail.com>
2023-12-18 01:16:41 +0200

    feat(queue): add timeouts

---
+++ config/queue.lua
@@ -0,0 +1,8 @@
+return {
+    ---Amount of seconds to wait before removing a player from the queue after disconnecting while waiting.
+    timeoutSeconds = 30,
+
+    ---Amount of seconds to wait before removing a player from the queue after disconnecting while installing server data.
+    ---Notice that an additional ~2 minutes will be waited due to limitations with how FiveM handles joining players.
+    joiningTimeoutSeconds = 0,
+}
