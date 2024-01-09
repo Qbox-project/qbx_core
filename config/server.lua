f3aed1cd2c7d00ecefbe2949137740768c02e16b
bicatcho11 <151503460+bicatcho11@users.noreply.github.com>
2024-01-09 08:42:32 +0300

    Adding a clear currency to the server
    Adding a clear currency to the server
    example $

--- config/server.lua
+++ config/server.lua
@@ -6,6 +6,7 @@ return {
         ---@alias Money {cash: number, bank: number, crypto: number}
         ---@type Money
         moneyTypes = { cash = 500, bank = 5000, crypto = 0 }, -- type = startamount - Add or remove money types for your server (for ex. blackmoney = 0), remember once added it will not be removed from the database!
+        currencyPrefix = '$', -- For example. $ for USD - The price will be in the foreground
         dontAllowMinus = { 'cash', 'crypto' }, -- Money that is not allowed going in minus
         paycheckTimeout = 10, -- The time in minutes that it will give the paycheck
         paycheckSociety = false -- If true paycheck will come from the society account that the player is employed at, requires qb-management
