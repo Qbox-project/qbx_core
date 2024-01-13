989f62aaf413a8c51e2f02965c55c13795039eb2
solareon <769465+solareon@users.noreply.github.com>
2024-01-13 07:47:04 +0000

    refactor: use callback for vehicle spawns

--- modules/lib.lua
+++ modules/lib.lua
@@ -170,7 +170,7 @@ if isServer then
         end, 5000)
 
         local netId = NetworkGetNetworkIdFromEntity(veh)
-        TriggerClientEvent('qbx_core:client:vehicleSpawned', owner, netId, props)
+        lib.callback.await('qbx_core:client:vehicleSpawned', owner, netId, props)
         return netId
     end
 else
