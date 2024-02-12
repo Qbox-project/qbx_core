002909dc377d0b1fb4a265b243c39a49e8ca5fff
makaman50 <159690615+makaman50@users.noreply.github.com>
2024-02-12 09:43:56 +0300

    Update shared.lua

--- config/shared.lua
+++ config/shared.lua
@@ -1,5 +1,10 @@
 return {
     serverName = 'Server',
     defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
-    notifyPosition = 'top-right' -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
+    notifyPosition = 'top-right', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
+    moneyTax = {
+        vehicle = 12, -- +12% of the price
+        goods = 3, -- +3% of the price
+        fuel = 5, -- +5% of the price
+    }
 }
