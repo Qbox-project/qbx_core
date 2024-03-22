131d575b0b9c2ae6419999655775d4c0676032f5
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-03-22 20:09:22 +0100

    I'm a fucking idiot

--- server/loops.lua
+++ server/loops.lua
@@ -6,8 +6,8 @@ local function removeHungerAndThirst(src, player)
     local newHunger = playerState.hunger - config.player.hungerRate
     local newThirst = playerState.thirst - config.player.thirstRate
 
-    player.Functions.SetMetaData('thirst', math.max(0, newHunger))
-    player.Functions.SetMetaData('hunger', math.max(0, newThirst))
+    player.Functions.SetMetaData('thirst', math.max(0, newThirst))
+    player.Functions.SetMetaData('hunger', math.max(0, newHunger))
 
     TriggerClientEvent('hud:client:UpdateNeeds', src, newHunger, newThirst)
     player.Functions.Save()
