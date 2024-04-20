94dc1279b8f22bfdf611b1f6c3d08288f82f4bb5
Scully <51968381+Scullyy@users.noreply.github.com>
2024-04-20 00:31:34 -0700

    Reverse hash for ox weapons

--- shared/main.lua
+++ shared/main.lua
@@ -3,13 +3,25 @@ qbShared.Items = require 'shared.items'
 qbShared.ForceJobDefaultDutyAtLogin = true -- true: Force duty state to jobdefaultDuty | false: set duty state from database last saved
 qbShared.Locations = require 'shared.locations'
 qbShared.Vehicles = require 'shared.vehicles'
-qbShared.Weapons = require 'shared.weapons'
 
 ---@type table<number, Vehicle>
 qbShared.VehicleHashes = {}
 
 for _, v in pairs(qbShared.Vehicles) do
 	qbShared.VehicleHashes[v.hash] = v
+end
+
+local weaponConfig = require '@ox_inventory.data.weapons'
+qbShared.Weapons = weaponConfig.Weapons
+qbShared.AmmoTypes = weaponConfig.Ammo
+
+---@type table<number, Weapon>
+qbShared.WeaponHashes = {}
+
+for k, v in pairs(qbShared.Weapons) do
+	v.name = k
+
+	qbShared.WeaponHashes[joaat(k)] = v
 end
 
 return qbShared
