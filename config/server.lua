f0129e113081a8340c4020d1df9bafcf4162c1ff
coblyox <55489629+coblyox@users.noreply.github.com>
2024-06-20 20:06:57 +0300

    tweaks

--- config/server.lua
+++ config/server.lua
@@ -31,8 +31,8 @@ return {
             AccountNumber = {
                 valueFunction = function()
                     return 'US0' ..
-                    math.random(1, 9) ..
-                    'QBX' .. math.random(1111, 9999) .. math.random(1111, 9999) .. math.random(11, 99)
+                        math.random(1, 9) ..
+                        'QBX' .. math.random(1111, 9999) .. math.random(1111, 9999) .. math.random(11, 99)
                 end,
             },
             PhoneNumber = {
@@ -87,7 +87,6 @@ return {
 
     characters = {
         playersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
-            ['license2:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'] = 5,
         },
 
         defaultNumberOfCharacters = 3, -- Define maximum amount of default characters (maximum 3 characters defined by default)
--- config/shared.lua
+++ config/shared.lua
@@ -5,23 +5,5 @@ return {
     ---@type { name: string, amount: integer, metadata: fun(source: number): table }[]
     starterItems = {              -- Character starting items
         { name = 'phone', amount = 1 },
-        {
-            name = 'id_card',
-            amount = 1,
-            metadata = function(source)
-                assert(GetResourceState('qbx_idcard') == 'started',
-                    'qbx_idcard resource not found. Required to give an id_card as a starting item')
-                return exports.qbx_idcard:GetMetaLicense(source, { 'id_card' })
-            end
-        },
-        {
-            name = 'driver_license',
-            amount = 1,
-            metadata = function(source)
-                assert(GetResourceState('qbx_idcard') == 'started',
-                    'qbx_idcard resource not found. Required to give an id_card as a starting item')
-                return exports.qbx_idcard:GetMetaLicense(source, { 'driver_license' })
-            end
-        },
     }
 }
