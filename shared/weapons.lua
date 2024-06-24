c25f71d47a1ad1f50c736c9d2002ed11fbe08795
solareon <769465+solareon@users.noreply.github.com>
2024-06-24 20:32:35 +0200

    refactor(shared): add deprecation warnings, item type def

--- shared/weapons.lua
+++ shared/weapons.lua
@@ -5,6 +5,7 @@
 ---@field ammotype? string
 ---@field damagereason string
 
+---@deprecated This file is deprecated and will be removed in the future. Please add your weapons in ox_inventory/data/weapons.lua file. Currently weapons in this file will be converted on next server start
 ---@type table<number, Weapon>
 return {
 	-- // WEAPONS
