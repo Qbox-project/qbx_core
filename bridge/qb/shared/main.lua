2e190bbc3325ab2f485ba607c969c38722dee650
Solareon <769465+solareon@users.noreply.github.com>
2024-08-12 17:59:10 +0200

    feat: create Shared.Items from oxinv
    Co-authored-by: PaPi <89279156+Mesrine67@users.noreply.github.com>

--- bridge/qb/shared/main.lua
+++ bridge/qb/shared/main.lua
@@ -1,5 +1,69 @@
 local qbShared = require 'shared.main'
 
+qbShared.Items = {}
+local oxItems = require '@ox_inventory.data.items'
+for item, data in pairs(oxItems) do
+    qbShared.Items[item] = {
+        name = item,
+        label = data.label,
+        weight = data.weight or 0,
+        type = 'item',
+        image = data.client?.image or string.strjoin(item,'.png'),
+        unique = false,
+        useable = true,
+        shouldClose = data.close or true,
+        combinable = nil,
+        description = data.description or nil
+    }
+end
+local oxWeapons = require '@ox_inventory.data.weapons'
+for weapon, data in pairs(oxWeapons.Weapons) do
+    weapon = string.lower(weapon)
+    qbShared.Items[weapon] = {
+        name = weapon,
+        label = data.label,
+        weight = data.weight,
+        type = 'weapon',
+        ammotype = data.ammoname or nil,
+        image = data.client?.image or string.strjoin(weapon,'.png'),
+        unique = true,
+        useable = false,
+        shouldClose = true,
+        combinable = nil,
+        description = nil
+    }
+end
+for component, data in pairs(oxWeapons.Components) do
+    component = string.lower(component)
+    qbShared.Items[component] = {
+        name = component,
+        label = data.label,
+        weight = data.weight,
+        type = 'component',
+        image = data.client?.image or string.strjoin(component,'.png'),
+        unique = true,
+        useable = false,
+        shouldClose = true,
+        combinable = nil,
+        description = data.description
+    }
+end
+for ammo, data in pairs(oxWeapons.Ammo) do
+    ammo = string.lower(ammo)
+    qbShared.Items[ammo] = {
+        name = ammo,
+        label = data.label,
+        weight = data.weight,
+        type = 'ammo',
+        image = data.client?.image or string.strjoin(ammo,'.png'),
+        unique = true,
+        useable = false,
+        shouldClose = true,
+        combinable = nil,
+        description = data.description
+    }
+end
+
 local starterItems = require 'config.shared'.starterItems
 ---@deprecated use starterItems in config/shared.lua
 qbShared.StarterItems = {}
