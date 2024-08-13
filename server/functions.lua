052e4e4909b79a1e321415223db39f1f90c9278f
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-08-12 23:47:28 -0400

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -348,7 +348,10 @@ function IsPlayerBanned(source)
         return true, ('You have been banned from the server:\n%s\nYour ban expires in %s/%s/%s %s:%s\n'):format(result.reason, timeTable.day, timeTable.month, timeTable.year, timeTable.hour, timeTable.min)
     else
         CreateThread(function()
-            if license2 then storage.deleteBan({ license = license2 }) end
+            if license2 then
+                storage.deleteBan({ license = license2 })
+            end
+
             storage.deleteBan({ license = license })
         end)
     end
