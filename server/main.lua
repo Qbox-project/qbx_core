c017f01ef21d76a30c48d176543e364e55e68479
Solareon <769465+solareon@users.noreply.github.com>
2024-08-14 13:29:56 +0200

    fix: lint errors

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,6 @@
 lib.versionCheck('Qbox-project/qbx_core')
-local startupErrors, errorMessage = lib.checkDependency('ox_lib', '3.20.0', true), 'ox_lib version 3.20.0 or higher is required'
-startupErrors, errorMessage = not startupErrors and lib.checkDependency('ox_inventory', '2.42.0', true), 'ox_inventory version 2.42.0 or higher is required'
+local startupErrors, errorMessage = lib.checkDependency('ox_lib', '3.20.0', true), 'ox_lib version 3.20.0 or higher is required' -- luacheck: ignore
+startupErrors, errorMessage = not startupErrors and lib.checkDependency('ox_inventory', '2.42.0', true), 'ox_inventory version 2.42.0 or higher is required' -- luacheck: ignore
 startupErrors, errorMessage = not startupErrors and GetConvar('inventory:framework', '') == 'qbx' and true, 'inventory:framework must be set to "qbx" in order to use qbx_core'
 if not startupErrors then
     lib.print.error('Startup errors detected, shutting down server...')
