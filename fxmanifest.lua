ebd3928a2ebf4acf734d44fc3946c617e11db1b5
Manason <Manason@users.noreply.github.com>
2024-09-01 02:05:19 +0000

    chore: bump manifest version to v1.18.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qbx_core'
 description 'The core resource for the Qbox Framework'
 repository 'https://github.com/Qbox-project/qbx_core'
-version '1.18.0'
+version '1.18.1'
 
 ox_lib 'locale'
 
