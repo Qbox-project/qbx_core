2aa90b5e585307b6aaa2dea487615eca7a197e1c
jxnnyo <jxnnyod@gmail.com>
2024-09-01 17:08:41 +0100

    chore: fix misleading dependency error

--- server/main.lua
+++ server/main.lua
@@ -3,7 +3,7 @@ local startupErrors, errorMessage
 if not lib.checkDependency('ox_lib', '3.20.0', true) then
     startupErrors, errorMessage = true, 'ox_lib version 3.20.0 or higher is required'
 elseif not lib.checkDependency('ox_inventory', '2.42.1', true) then
-    startupErrors, errorMessage = true, 'ox_inventory version 2.42.0 or higher is required'
+    startupErrors, errorMessage = true, 'ox_inventory version 2.42.1 or higher is required'
 elseif GetConvar('inventory:framework', '') ~= 'qbx' then
     startupErrors, errorMessage = true, 'inventory:framework must be set to "qbx" in order to use qbx_core'
 end
