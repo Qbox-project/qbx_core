9ac3508f7be56c7d6c3771c83ec7a5ccfda26d2e
David Malchin <malchin459@gmail.com>
2024-09-27 10:49:32 +0300

    fix(bridge/client): pass vehicleId to `hasKeys`

--- config/client.lua
+++ config/client.lua
@@ -273,7 +273,7 @@ return {
     },
 
     --- Only used by QB bridge
-    hasKeys = function()
-        return exports.qbx_vehiclekeys:HasKeys()
+    hasKeys = function(vehicleId)
+        return exports.qbx_vehiclekeys:HasKeys(vehicleId)
     end,
 }
