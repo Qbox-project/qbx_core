9d84dae748e76c9476e50cc2017fa7bda05eb8d7
JustShoveIt <himadri.dares@gmail.com>
2024-10-16 04:03:12 +0800

    change notify position

--- config/shared.lua
+++ config/shared.lua
@@ -1,9 +1,9 @@
 return {
     serverName = 'Ornate Roleplay',
     defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
-    notifyPosition = 'left', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
+    notifyPosition = 'center-left', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
     ---@type { name: string, amount: integer, metadata: fun(source: number): table }[]
-    starterItems = {         -- Character starting items
+    starterItems = {                -- Character starting items
         { name = 'phone', amount = 1 },
         {
             name = 'id_card',
