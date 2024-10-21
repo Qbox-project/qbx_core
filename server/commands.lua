372ad6dfabda85613ff4f32fb042b9737cf2a7ba
Matthew <22198949+mafewtm@users.noreply.github.com>
2024-10-21 16:34:38 -0400

    refactor(server/commands): use chat export instead (#599)

--- server/commands.lua
+++ server/commands.lua
@@ -342,20 +342,20 @@ lib.addCommand('ooc', {
     local playerCoords = GetEntityCoords(GetPlayerPed(source))
     for _, v in pairs(players) do
         if v == source then
-            TriggerClientEvent('chat:addMessage', v --[[@as Source]], {
+            exports.chat:addMessage(v --[[@as Source]], {
                 color = { 0, 0, 255},
                 multiline = true,
                 args = {('OOC | %s'):format(GetPlayerName(source)), message}
             })
         elseif #(playerCoords - GetEntityCoords(GetPlayerPed(v))) < 20.0 then
-            TriggerClientEvent('chat:addMessage', v --[[@as Source]], {
+            exports.chat:addMessage(v --[[@as Source]], {
                 color = { 0, 0, 255},
                 multiline = true,
                 args = {('OOC | %s'):format(GetPlayerName(source)), message}
             })
         elseif IsPlayerAceAllowed(v --[[@as string]], 'admin') then
             if IsOptin(v --[[@as Source]]) then
-                TriggerClientEvent('chat:addMessage', v --[[@as Source]], {
+                exports.chat:addMessage(v--[[@as Source]], {
                     color = { 0, 0, 255},
                     multiline = true,
                     args = {('Proximity OOC | %s'):format(GetPlayerName(source)), message}
