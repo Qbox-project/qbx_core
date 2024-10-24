e0d6eae68ee9c125572b7a3f1dba099049c1da6a
zozomanx <foxfire2000z@gmail.com>
2024-10-23 21:17:16 -0400

    feat: add player ID to player load log (#605)

--- server/character.lua
+++ server/character.lua
@@ -49,7 +49,7 @@ lib.callback.register('qbx_core:server:loadCharacter', function(source, citizenI
         color = 'green',
         message = ('**%s** (%s |  ||%s|| | %s | %s | %s) loaded'):format(GetPlayerName(source), GetPlayerIdentifierByType(source, 'discord') or 'undefined', GetPlayerIdentifierByType(source, 'ip') or 'undefined', GetPlayerIdentifierByType(source, 'license2') or GetPlayerIdentifierByType(source, 'license') or 'undefined', citizenId, source)
     })
-    lib.print.info(('%s (Citizen ID: %s) has successfully loaded!'):format(GetPlayerName(source), citizenId))
+    lib.print.info(('%s (Citizen ID: %s ID: %s) has successfully loaded!'):format(GetPlayerName(source), citizenId, source))
 end)
 
 ---@param data unknown
