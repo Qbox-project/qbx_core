0b7ed08284cbb491ee056e006dbd568cd6c9faaf
GoldO <83804899+GoldO-dev@users.noreply.github.com>
2024-11-05 17:30:08 +0100

    refactor(modules/lib): enhance draw text functions for better flexibility

--- modules/lib.lua
+++ modules/lib.lua
@@ -325,6 +325,8 @@ else
     ---@field scale? integer default: `0.35`
     ---@field font? integer default: `4`
     ---@field color? vector4 rgba, white by default
+    ---@field enableDropShadow? boolean 
+    ---@field enableOutline? boolean 
 
     ---@class LibDrawText2DParams : LibDrawTextParams
     ---@field coords vector2
@@ -341,12 +343,19 @@ else
         local color = params.color or vec4(255, 255, 255, 255)
         local width = params.width or 1.0
         local height = params.height or 1.0
+        local enableDropShadow = params.enableDropShadow or false
+        local enableOutline = params.enableOutline or false
 
         SetTextScale(scale, scale)
         SetTextFont(font)
         SetTextColour(math.floor(color.r), math.floor(color.g), math.floor(color.b), math.floor(color.a))
-        SetTextDropShadow()
-        SetTextOutline()
+        if enableDropShadow then
+            SetTextDropShadow()
+        end
+        if enableOutline then
+            SetTextOutline()
+        end
+
         SetTextCentre(true)
         BeginTextCommandDisplayText('STRING')
         AddTextComponentSubstringPlayerName(text)
@@ -356,19 +365,31 @@ else
     ---@class LibDrawText3DParams : LibDrawTextParams
     ---@field coords vector3
     ---@field disableDrawRect? boolean
+    ---@field scale? integer | vector2 default: `vec2(0.35,0.35)`
 
     ---Draws text onto the screen in 3D space for a single frame.
     ---@param params LibDrawText3DParams
     function qbx.drawText3d(params) -- luacheck: ignore
+        local isScaleparamANumber = type(params.scale) == "number"
         local text = params.text
         local coords = params.coords
-        local scale = params.scale or 0.35
+        local scale = (isScaleparamANumber and vec2(params.scale, params.scale))
+                  or params.scale
+                  or vec2(0.35, 0.35)
         local font = params.font or 4
         local color = params.color or vec4(255, 255, 255, 255)
+        local enableDropShadow = params.enableDropShadow or false
+        local enableOutline = params.enableOutline or false
 
-        SetTextScale(scale, scale)
+        SetTextScale(scale.x, scale.y)
         SetTextFont(font)
         SetTextColour(math.floor(color.r), math.floor(color.g), math.floor(color.b), math.floor(color.a))
+        if enableDropShadow then
+            SetTextDropShadow()
+        end
+        if enableOutline then
+            SetTextOutline()
+        end
         SetTextCentre(true)
         BeginTextCommandDisplayText('STRING')
         AddTextComponentSubstringPlayerName(text)
