58d862e406014bc8aed1ac4db0ddd90d1a6ae867
Manason <clanerp@gmail.com>
2024-11-28 08:17:38 -0800

    fix(vehicle-persistence): use diff provided tyres changes

--- server/vehicle-persistence.lua
+++ server/vehicle-persistence.lua
@@ -65,14 +65,7 @@ RegisterNetEvent('qbx_core:server:vehiclePropsChanged', function(netId, diff)
     end
 
     if diff.tyres then
-        local damage = {}
-        for i = 0, 7 do
-            if IsVehicleTyreBurst(vehicle, i, false) then
-                damage[i] = IsVehicleTyreBurst(vehicle, i, true) and 2 or 1
-            end
-        end
-
-        props.tyres = damage
+        props.tyres = diff.tyres ~= 'deleted' and diff.tyres or nil
     end
 
     exports.qbx_vehicles:SaveVehicle(vehicle, {
