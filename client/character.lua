d006715131ae1c6f480c1a8a5fb80a742cf74b64
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-02 11:24:09 -0500

    fix(client): ensure tutorial sessions

--- client/character.lua
+++ client/character.lua
@@ -102,6 +102,8 @@ local randomPeds = {
     }
 }
 
+NetworkStartSoloTutorialSession()
+
 local nationalities = {}
 
 if config.characters.limitNationalities then
@@ -370,8 +372,10 @@ local function chooseCharacter()
     SetEntityCoords(cache.ped, randomLocation.pedCoords.x, randomLocation.pedCoords.y, randomLocation.pedCoords.z, false, false, false, false)
     SetEntityHeading(cache.ped, randomLocation.pedCoords.w)
 
-    if not NetworkIsInTutorialSession() then
-        NetworkStartSoloTutorialSession()
+    NetworkStartSoloTutorialSession()
+
+    while not NetworkIsInTutorialSession() do
+        Wait(0)
     end
 
     Wait(1500)
