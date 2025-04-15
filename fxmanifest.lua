b8f4f4a1e8a9628e2c61184c0eac5d09612d295d
ChatDisabled <ChatDisabled@users.noreply.github.com>
2025-04-15 21:25:52 +0000

    chore: bump manifest version to v1.23.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qbx_core'
 description 'The core resource for the Qbox Framework'
 repository 'https://github.com/Qbox-project/qbx_core'
-version '1.22.5'
+version '1.23.0'
 
 ox_lib 'locale'
 
