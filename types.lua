18f8d5618a19dad6da21e3462f08fff97e84376d
sw4m <56038988+sw4m@users.noreply.github.com>
2025-05-24 19:00:04 +0200

    Update licences.driver to table

--- types.lua
+++ types.lua
@@ -185,7 +185,7 @@
 ---@field fingerprint string
 ---@field walletid string
 ---@field criminalrecord {hasRecord: boolean, date?: table} TODO: date is os.date(), create better type than table
----@field licences {id: boolean, driver: boolean, weapon: boolean}
+---@field licences {id: boolean, driver: {car: boolean, motorbike: boolean, cdl: boolean}, weapon: boolean}
 ---@field inside {house?: any, apartment: {apartmentType?: any, apartmentId?: integer}} TODO: expand
 ---@field phonedata {SerialNumber: string, InstalledApps: table} TODO: expand
 ---@field [string] any
