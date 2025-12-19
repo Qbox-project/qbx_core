8a066c43ed10115bcb29f5fb30852150ae499de4
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2025-12-19 15:49:16 +0100

    bypass optin function directly

--- server/functions.lua
+++ server/functions.lua
@@ -343,6 +343,7 @@ exports('GetPermission', GetPermission)
 ---@param source Source
 ---@return boolean
 function IsOptin(source)
+    if not serverConfig.requireOptIn then return true end
     local license = GetPlayerIdentifierByType(source --[[@as string]], 'license2') or GetPlayerIdentifierByType(source --[[@as string]], 'license')
     if not license or not IsPlayerAceAllowed(source --[[@as string]], 'admin') then return false end
     local player = GetPlayer(source)
